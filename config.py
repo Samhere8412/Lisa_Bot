45aa3b6f353e378be59df19f761d1d7becdb4e23
Samhere8412 <117311816+Samhere8412@users.noreply.github.com>
2025-01-13 02:32:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "14050586"
-API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
+API_ID = "21394317"
+API_HASH = "67d47811e48343bb7ef1d098246defce"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "5465943450"
-SUPPORT_GRP = "LOVE_FEELINGS_WILL1"
-UPDATE_CHNL = "alonegroup121"
-OWNER_USERNAME = "dil_sagar_121"
+OWNER_ID = "2073178010"
+SUPPORT_GRP = "https://t.me/Global_CHATTING_HINDI_ZONE"
+UPDATE_CHNL = "https://t.me/Global_CHATTING_HINDI_ZONE"
+OWNER_USERNAME = "KingOfHellLuci"
